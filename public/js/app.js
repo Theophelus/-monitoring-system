bc6a24a13cc3b5a16b71ee7769d71b2f3cd508cf
Theophelus <aneletom10@gmail.com>
2018-11-21 18:34:31 +0200

    adding project api and refactoring client side functionality

--- public/js/app.js
+++ public/js/app.js
@@ -53,7 +53,6 @@ let renderLatestRepos = new Vue({
                     }
                 })
     }
-
 });
 
 
--- server.js
+++ server.js
@@ -57,7 +57,20 @@ app.get('/api/get/students', async function (req, res) {
     })
 })
 
-
+//Define an api to get all projects
+app.get('/api/get/projects', async (req, res) => {
+    let results = await pool.query('select * from products');
+    if (results.rowCount === 0) {
+        return res.json({
+            success: false,
+            data: []
+        })
+    }
+    return res.json({
+        success: true,
+        data: found.rows
+    });
+})
 app.get('/api/get/github/:username', async function (req, res) {
     const {
         username
