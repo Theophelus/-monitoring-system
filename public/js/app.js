79ca83601072ff9bdd4cfc8a59e83f5f36958ee2
Theophelus <aneletom10@gmail.com>
2018-11-21 15:33:23 +0200

    commit rectifying mounted function and extracting keys we want to use from the api

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+
+
 let renderLatestRepos = new Vue({
     el: '.renderLatestRepos',
     data: {
@@ -36,8 +38,16 @@ let renderLatestRepos = new Vue({
                         for (let current of repos) {
                             axios
                                 .get(`/api/get/lastest/repos/${current.github_username}`)
-                                .then(function (results) {    
-                                    self.repos.push(results.data.lastestRepos[0]);
+                                .then(function (results) {
+                                    let name = results.data.lastestRepos[0].full_name.split('/'); 
+                                    const {created_at,updated_at} = results.data.lastestRepos[0];
+                                    let data = {
+                                        full_name: name[0],
+                                        project_name : name[1],
+                                        created_at:created_at,
+                                        updated_at:updated_at
+                                    }
+                                    self.repos.push(data);
                                 });
                         }
                     }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+
 // const flash = require('express-flash');
 // const session = require('express-session');
 
