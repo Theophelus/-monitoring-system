9707e60bda56c6810e15b9608346d3c0be1075ae
Mr Booi <ayabongabooi2@gmail.com>
2018-11-22 16:16:30 +0200

    clean code

---
+++ public/components/main.js
@@ -0,0 +1,27 @@
+let app = new Vue ({
+    el: '#renderLatestRepos',    
+    data: {
+       userRepos : []
+    },
+
+    mounted: function() {
+        let self = this;
+        axios.get('/api/get/repos/latest')
+            .then(function (results) {
+                if (results.data.success) {
+                    let data = results.data.data;
+                    for (const current of data) {
+                        self.userRepos.push(current)
+                    }
+                }
+            });
+    },
+
+    methods: {
+          
+       
+
+    }
+
+
+})
