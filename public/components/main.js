91702a0abc5acb7036fc8906950a8c99e29850bc
Theophelus <aneletom10@gmail.com>
2018-11-23 09:54:08 +0200

    adding codewars Ranks

--- public/components/main.js
+++ public/components/main.js
@@ -1,26 +1,46 @@
-let app = new Vue ({
-    el: '#renderLatestRepos',    
+let app = new Vue({
+    el: '#renderLatestRepos',
     data: {
-       userRepos : []
+        userRepos: [],
+        userRank: []
     },
-
-    mounted: function() {
+    mounted: function () {
         let self = this;
         axios.get('/api/get/repos/latest')
             .then(function (results) {
-                console.log(results.data)
+                // console.log(results.data)
+                // console.log(results)
                 if (results.data.success) {
                     let data = results.data.data;
                     for (const current of data) {
+                        // console.log(current);
+
                         self.userRepos.push(current)
                     }
                 }
             });
+
+            axios.get('/api/coderwars/users/rank/MrBooi')
+                .then(function (results) {
+                    if (results.data.success) {
+                        let data = results.data.data;
+    
+                        let getWars = {
+                            username: data.name,
+                            TotolCompeted: data.codeChallenges.totalCompleted,
+                            honor: data.honor,
+                        }
+                       
+                        self.userRank.push(getWars)
+    
+                    }
+                    
+                })
     },
 
     methods: {
-          
-       
+
+
 
     }
 
--- public/components/rank.js
+++ public/components/rank.js
@@ -1,36 +1,52 @@
-VTTCue.component({
-    data: () => {
+Vue.component('rank-wars', {
+    props: ['rank'],
+    data: function () {
         return {
             getCodeWarRanks: []
         }
     },
-    mounted: function() {
 
+    mounted: function () {
+        let self = this;
+        axios.get('/api/coderwars/users/rank/MrBooi')
+            .then(function (results) {
+                if (results.data.success) {
+                    let data = results.data.data;
+
+                    let getWars = {
+                        username: data.name,
+                        TotolCompeted: data.codeChallenges.totalCompleted,
+                        honor: data.honor,
+                    }
+                   
+                    self.getCodeWarRanks.push(getWars)
+
+                }
+                
+            })
     },
-    template:  `
+
+
+    template: `
     <div class="panel-body">
-    
     <table class="table table-striped">
         <thead>
             <tr>
                 <th>#</th>
-                <th>FUsername</th>
+                <th>Username</th>
                 <th>Totol Completed</th>
                 <th>Language</th>
-                <th>Updated</th>
+               
             </tr>
         </thead>
         <tbody>
-            <tr v-for="repo in repos">
+            <tr v-for="userRank in rank">
                 <td>1</td>
-                <td> {{repo.full_name}} </td>
-                <td> {{repo.project_name}} </td>
-                <td> {{repo.created_at}} </td>
-                <td> {{repo.updated_at}} </td>
+                <td> {{userRank.username}} </td>
+                <td> {{userRank.TotolCompeted}} </td>
+                <td> {{userRank.honor}} </td>
             </tr>
         </tbody>
     </table>
-</div>
-
-    `
+</div>`
 });
